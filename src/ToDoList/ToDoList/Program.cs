a7d271e14fb37a62061ecc2d2d1ca942c047a5b6
TiagoFSDC <tiagofsdc@gmail.com>
2023-03-26 22:27:24 -0300

    Criação dos metodos

--- src/ToDoList/ToDoList/Program.cs
+++ src/ToDoList/ToDoList/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System.ComponentModel.Design;
+using ToDoList;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
+        List<ToDo> listTodo = new List<ToDo>(); 
         int option = 0;
         do
         {
@@ -14,10 +16,13 @@ internal class Program
                     Console.WriteLine("Opção inválida");
                     break;
                 case 1:
+                    CreateTask();
                     break;
                 case 2:
+                    RemoveTask();
                     break;
                 case 3:
+                    EditTask();
                     break;
                 case 4:
                     break;
@@ -25,13 +30,25 @@ internal class Program
         }while (option != 4);
     }
 
+    private static void RemoveTask()
+    {
+    }
+
+    private static void CreateTask()
+    {
+    }
+
+    private static void EditTask()
+    {
+    }
+
     private static int Menu(int options)
     {
-        Console.WriteLine("Menu de opções");
-        Console.WriteLine("1 - Adicionar tarefa");
-        Console.WriteLine("2- Remover tarefa");
-        Console.WriteLine("3 - Editar tarefa");
-        Console.WriteLine("4 - Sair");
+        Console.WriteLine("Menu de opções ");
+        Console.WriteLine("1 - Adicionar tarefa ");
+        Console.WriteLine("2- Remover tarefa ");
+        Console.WriteLine("3 - Editar tarefa ");
+        Console.WriteLine("4 - Sair ");
         Console.Write("Escolha uma das opções: ");
         options = int.Parse(Console.ReadLine());
         return options;
