ea4edbf081f63f705559aea7008615640de2bc8c
TiagoFSDC <tiagofsdc@gmail.com>
2023-03-26 22:27:46 -0300

    Criação dos metodos

--- src/ToDoList/ToDoList/Program.cs
+++ src/ToDoList/ToDoList/Program.cs
@@ -5,7 +5,13 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        List<ToDo> listTodo = new List<ToDo>(); 
+        List<ToDo> listTodo = new List<ToDo>();
+        List<Person> pessoas = new List<Person>();
+        List<Category> categorias = new List<Category>();
+
+        //LoadFromFile(listTodo);
+        //LoadFromFile(categorias);
+
         int option = 0;
         do
         {
@@ -22,12 +28,30 @@ internal class Program
                     RemoveTask();
                     break;
                 case 3:
-                    EditTask();
+                    MenuTask();
                     break;
                 case 4:
+                    System.Environment.Exit(0);
                     break;
             }
         }while (option != 4);
+    }
+
+
+
+    private static void MenuTask()
+    {
+        int options;
+        Console.WriteLine("Menu de opções para edição das tarefas");
+        Console.WriteLine("1 - Marcar tarefa como concluída");
+        Console.WriteLine("2 - Mudar a tarefa de concluída para não concluída");
+        Console.WriteLine("3 - Editar Pessoa");
+        Console.WriteLine("4 - Mudar categorias da tarefa");
+        Console.WriteLine("5 - Sair");
+    }
+
+    private static void LoadFromFile()
+    {
     }
 
     private static void RemoveTask()
@@ -38,18 +62,15 @@ internal class Program
     {
     }
 
-    private static void EditTask()
-    {
-    }
-
     private static int Menu(int options)
     {
-        Console.WriteLine("Menu de opções ");
-        Console.WriteLine("1 - Adicionar tarefa ");
-        Console.WriteLine("2- Remover tarefa ");
-        Console.WriteLine("3 - Editar tarefa ");
-        Console.WriteLine("4 - Sair ");
-        Console.Write("Escolha uma das opções: ");
+        int option;
+        Console.WriteLine("Menu de opções");
+        Console.WriteLine("1 - Adicionar tarefa");
+        Console.WriteLine("2- Remover tarefa");
+        Console.WriteLine("3 - Editar tarefa");
+        Console.WriteLine("4 - Sair");
+        Console.Write("Escolha uma das opções:");
         options = int.Parse(Console.ReadLine());
         return options;
     }
