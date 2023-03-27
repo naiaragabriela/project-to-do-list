a65567ed2a72a28e168799462a0d177bc5e14da9
TiagoFSDC <tiagofsdc@gmail.com>
2023-03-26 21:38:06 -0300

    Criaçao do menu

--- src/ToDoList/ToDoList/Program.cs
+++ src/ToDoList/ToDoList/Program.cs
@@ -1,3 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-.
+﻿using System.ComponentModel.Design;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int option = 0;
+        do
+        {
+            option = Menu(option);
+            switch (option)
+            {
+                default:
+                    Console.WriteLine("Opção inválida");
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+            }
+        }while (option != 4);
+    }
+
+    private static int Menu(int options)
+    {
+        Console.WriteLine("Menu de opções");
+        Console.WriteLine("1 - Adicionar tarefa");
+        Console.WriteLine("2- Remover tarefa");
+        Console.WriteLine("3 - Editar tarefa");
+        Console.WriteLine("4 - Sair");
+        Console.Write("Escolha uma das opções: ");
+        options = int.Parse(Console.ReadLine());
+        return options;
+    }
+}
